19ae751f2973b4f8a896a094ff610af445848383
Carlos Regalado <cd.regaladoaguirre@hotmail.com>
2021-10-13 17:41:50 -0500

    Added starting files

---
+++ main.js
@@ -0,0 +1,21 @@
+const books = [];
+
+const book = function(title, author, pages, isRead){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = isRead;
+}
+book.prototype.info = function(){
+    return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;
+};
+
+function addBookToLibrary(book){
+    books.push(book);
+}
+
+const theHobbit = new book("The hobbit", "J.R.R. Tolkien", 293, "not read yet");
+
+addBookToLibrary(theHobbit);
+
+console.log(books)
